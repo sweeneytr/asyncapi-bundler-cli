4b852123ff24822da7be654328d16c5b43f6ca39
Tristan Sweeney <tsweeney@dustidentity.com>
2023-01-25 14:54:05 -0500

    Make cli print to stdout, ditch figlet

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
 #! /usr/bin/env node
 
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import bundle from '@asyncapi/bundler';
-import figlet from 'figlet';
 import { Command } from "commander";
 import path from "path";
 
-console.log(figlet.textSync("AsyncAPI Bundler"));
-
 const program = new Command();
 program
-  .version("0.3.0")
+  .version("0.4.0")
   .description("A CLI for leveraging @asyncapi/bundler")
   .argument('<input>', 'Asyncapi to bundle')
-  .argument('<output>', 'Bundled asyncapi file')
-  .action(async (input: string, output: string): Promise<void> => {
+  .action(async (input: string): Promise<void> => {
     try {
-      const document = await bundle([readFileSync(path.resolve(__dirname, input), 'utf-8')], {
+      const file_path = path.resolve(__dirname, input);
+      process.chdir(path.dirname(file_path));
+
+      const document = await bundle([readFileSync(path.basename(file_path), 'utf-8')], {
         referenceIntoComponents: true,
       });
 
-      writeFileSync(path.resolve(__dirname, output), document.yml());
+      console.dir(document.json(), { depth: null });
     } catch(e) {
       console.error(e);
     }
